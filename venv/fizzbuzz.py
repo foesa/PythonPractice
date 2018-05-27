40d83ffcd7cc4227a511fffb9a4394615261f380
Foesa <leguavoen@gmail.com>
2018-05-27 15:43:10 +0100

    changed input stuff

--- venv/fizzbuzz.py
+++ venv/fizzbuzz.py
@@ -1,30 +1,32 @@
 
-//fizzcheck//
+"""fizzcheck"""
 def fizzcheck(x):
     truecheck = False
     if(x % fizz == 0):
         return(truecheck == True)
     else:
         return(truecheck)
-//buzzcheck//
+"""buzzcheck"""
 def buzzcheck(x):
     truecheck = False
     if(x % buzz == 0):
         return(truecheck==True)
     else:
         return(truecheck==False)
-//mainline//
-fizz = println("Please enter the number for fizz: ")
-buzz = println("Please enter the number for buzz: ")
+"""mainline"""""
+fizz = input("Please enter the number for fizz: ")
+buzz = input("Please enter the number for buzz: ")
 fizzbuzz = fizz*buzz
 for count in range(0,100):
     boolfizz = fizzcheck(count)
     boolbuzz = buzzcheck(count)
-    if(boolfizz == True && boolfizz==boolbuzz):
+    if(boolfizz == True & boolfizz==boolbuzz):
         print("FizzBuzz!")
-    elif((boolfizz == True && boolfizz!=boolbuzz):
+    elif(boolfizz == True & boolfizz!=boolbuzz):
         print("Fizz!")
-    elif((boolfizz != True && boolfizz!=boolbuzz)):
+    elif((boolfizz != True & boolfizz!=boolbuzz)):
         print("Buzz!")
     else:
         print(count)
+end
+
